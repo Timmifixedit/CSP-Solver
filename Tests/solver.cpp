2a7f95836d2c11f8deb75aa7bf8dd39606344fd3
Timmifixedit <tim.luchterhand@uni-ulm.de>
2020-07-24 17:38:38 +0200

    added ac3, createCsp from Constraints, Test for latter function

--- Tests/solver.cpp
+++ Tests/solver.cpp
@@ -30,3 +30,35 @@ TEST(solver_test, remove_inconsistent_indicate_removal) {
     EXPECT_TRUE(removeInconsistent(a));
     EXPECT_FALSE(removeInconsistent(a));
 }
+
+TEST(solver_test, create_csp_from_constraints) {
+    using namespace csp::util;
+    auto varA = std::make_shared<TestVar>(std::list{2, 3, 1});
+    auto varB = std::make_shared<TestVar>(std::list{2, 3, 1});
+    auto varC = std::make_shared<TestVar>(std::list{2, 3, 1});
+    TestConstraint aLessB(varA, varB, std::less<>());
+    TestConstraint aLessC(varA, varC, std::less<>());
+    TestConstraint bNotC(varB, varC, [](int lhs, int rhs) {return lhs != rhs;});
+    Csp problem = createCsp(std::list{aLessB, aLessC, bNotC});
+    EXPECT_EQ(problem.arcs.size(), 6);
+
+    auto testArcsPerVar = [&problem] (const std::shared_ptr<TestVar> &var) {
+        const auto &incomingArcs = problem.incomingNeighbours[var];
+        EXPECT_EQ(incomingArcs.size(), 2);
+        EXPECT_NE(incomingArcs[0].from(), incomingArcs[1].from());
+        for (const auto &arc : incomingArcs) {
+            EXPECT_EQ(arc.to(), var);
+            EXPECT_NE(arc.from(), var);
+            auto it = std::find_if(problem.arcs.begin(), problem.arcs.end(), [&arc](const TestArc &t) {
+                return arc.from() == t.from() && arc.to() == t.to();
+            });
+            EXPECT_NE(it, problem.arcs.end());
+            problem.arcs.erase(it);
+        }
+    };
+
+    testArcsPerVar(varA);
+    testArcsPerVar(varB);
+    testArcsPerVar(varC);
+    EXPECT_TRUE(problem.arcs.empty());
+}
--- src/solver.h
+++ src/solver.h
@@ -4,7 +4,9 @@
 
 #ifndef CSP_SOLVER_SOLVER_H
 #define CSP_SOLVER_SOLVER_H
-#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <list>
 
 #include "Variable.h"
 #include "Arc.h"
@@ -34,6 +36,53 @@ namespace csp::util {
         }
 
         return removed;
+    }
+
+    template<typename T, typename Predicate>
+    struct Csp {
+        using ArcT = Arc<T, Predicate>;
+        Csp() = default;
+        Csp(std::list<ArcT> arcs, std::unordered_map<VarPtr<T>, std::vector<ArcT>> incomingNeighbours) :
+            arcs(std::move(arcs)), incomingNeighbours(std::move(incomingNeighbours)) {}
+        std::list<ArcT> arcs;
+        std::unordered_map<cVarPtr<T>, std::vector<ArcT>> incomingNeighbours;
+    };
+
+    template<typename T, typename Predicate>
+    auto createCsp(const std::list<Constraint<T, Predicate>> &constraints) -> Csp<T, Predicate> {
+        Csp<T, Predicate> ret;
+        for (const auto &constraint : constraints) {
+            auto [normal, reversed] = constraint.getArcs();
+            ret.arcs.push_back(normal);
+            ret.arcs.push_back(reversed);
+            ret.incomingNeighbours[normal.from()].emplace_back(std::move(reversed));
+            ret.incomingNeighbours[normal.to()].emplace_back(std::move(normal));
+        }
+
+        return ret;
+    }
+
+    template<typename T, typename Predicate>
+    bool ac3(const Csp<T, Predicate> &problem) {
+        using ArcT = Arc<T, Predicate>;
+        std::list<ArcT> arcs = problem.arcs;
+        while (!arcs.empty()) {
+            const ArcT current = arcs.front();
+            arcs.pop_front();
+            if(removeInconsistent(current)) {
+                if (current.from()->valueDomain().empty()) {
+                    return false;
+                }
+
+                auto it = problem.incomingNeighbours.find(current.from());
+                assert(it != problem.incomingNeighbours.end());
+                std::copy_if(it->seond.bein(), it->second.end(), std::back_inserter(arcs), [&current](const auto &var) {
+                    var != current.to();
+                });
+            }
+        }
+
+        return true;
     }
 }
 
