04c9ec6518c6afa1dc2a29af961324c6b4adedc6
Timmifixedit <tim.luchterhand@uni-ulm.de>
2020-07-24 14:54:22 +0200

    adapted test program to new namespace

--- src/main.cpp
+++ src/main.cpp
@@ -11,19 +11,18 @@ using TestVar = csp::Variable<int>;
 using TestArc = csp::Arc<int, IntComperator>;
 
 
-
 int main() {
     auto a = std::make_shared<TestVar>(std::list{1, 2, 3});
     auto b = std::make_shared<TestVar>(std::list{1, 2, 3});
     TestArc alb(a, b, std::less<>());
-    csp::removeInconsistent(alb);
+    csp::util::removeInconsistent(alb);
     for (auto val : a->valueDomain()) {
         std::cout << val << " ";
     }
 
     std::cout << std::endl;
     alb.reverse();
-    csp::removeInconsistent(alb);
+    csp::util::removeInconsistent(alb);
     for (auto val : b->valueDomain()) {
         std::cout << val << " ";
     }
