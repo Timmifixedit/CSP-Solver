66520ee63595e29443ce0f20341ca627a5763157
Timmifixedit <tim.luchterhand@uni-ulm.de>
2020-08-12 20:37:45 +0200

    Backtracking test

--- Tests/solver.cpp
+++ Tests/solver.cpp
@@ -8,6 +8,12 @@
 #include "TestTypes.h"
 #include "solver.h"
 
+class NamedVar : public csp::Variable<int> {
+public:
+    NamedVar(std::list<int> domain, std::string name) : csp::Variable<int>(std::move(domain)), name(std::move(name)) {}
+    const std::string name;
+};
+
 TEST(solver_test, simple_csp) {
     auto varA = std::make_shared<TestVar>(std::list{2, 3, 1});
     auto varB = std::make_shared<TestVar>(std::list{2, 3, 1});
@@ -42,4 +48,44 @@ TEST(solver_test, impossible_csp) {
     TestConstraint bNotC(varB, varC, std::not_equal_to<>());
     csp::Csp problem = csp::make_csp(std::array{varA, varB, varC}, std::array{aNotB, aNotC, bNotC});
     EXPECT_FALSE(csp::solve(problem));
+}
+
+TEST(solver_test, backtrack) {
+    auto varA = std::make_shared<NamedVar>(std::list{1, 2, 3}, "A");
+    auto varB = std::make_shared<NamedVar>(std::list{1, 2, 3}, "B");
+    auto varC = std::make_shared<NamedVar>(std::list{1, 2, 3}, "C");
+    auto varD = std::make_shared<NamedVar>(std::list{3, 2, 1}, "D");
+    csp::Constraint ab(varA, varB, std::not_equal_to<>());
+    csp::Constraint ac(varA, varC, std::not_equal_to<>());
+    csp::Constraint bc(varB, varC, std::not_equal_to<>());
+    csp::Constraint bd(varB, varD, std::not_equal_to<>());
+    csp::Constraint cd(varC, varD, std::not_equal_to<>());
+    csp::Csp problem = csp::make_csp(std::array{varA, varB, varC, varD},
+                                     std::array{ab, ac, bc, bd, cd});
+
+    // Makes sure that Variable A is assigned first, then Variable C. Given the ordering of the values in C's domain
+    // this forces the solve algorithm to backtrack twice until 1 is selected for C.
+    auto aThenCStrat = [](const auto &csp) {
+        for (const auto &var : csp.variables) {
+            if (!var->isAssigned()) {
+                if (var->name == "A") {
+                    return var;
+                }
+
+                if(var->name == "D") {
+                    return var;
+                }
+
+                return var;
+            }
+        }
+
+        return csp.variables.front();
+    };
+
+    EXPECT_TRUE(csp::solve(problem, aThenCStrat));
+    EXPECT_EQ(varA->valueDomain().front(), 1);
+    EXPECT_EQ(varB->valueDomain().front(), 2);
+    EXPECT_EQ(varC->valueDomain().front(), 3);
+    EXPECT_EQ(varD->valueDomain().front(), 1);
 }
